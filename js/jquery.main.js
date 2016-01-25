1b0795bc5ee1147b9bfa4ef6805e035f82093e7b
Sergey <kuzmenkos1986@gmail.com>
2016-01-25 23:12:29 +0200

    one block need

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,32 +1,55 @@
 $(function(){
 
+    $('.swiper-container').each(function () {
+        Slider($(this));
+    });
+
+
 } );
 
-var Shablon = function (obj) {
-    this.obj = obj;
+var Slider = function (obj) {
 
+    //private properties
+    var _self = this,
+        _paginator = obj.find($('.slider__icon')),
+        _obj = obj;
 
-    this.init();
-};
-Shablon.prototype = {
-    init: function () {
-        var self = this;
+    //private methods
+    var _addEvents = function () {
 
-        self.core = self.core();
-        self.core.build();
-    },
-    core: function () {
-        var self = this;
-
-        return {
-            addEvents: function () {
-
-            },
-            build: function () {
-                self.core.addEvents();
-            }
+        },
+        _init = function () {
+            _addEvents();
         };
+    if (_obj.hasClass('slider__wrap')) {
+        var _swiperPromo = new Swiper(_obj, {
+            slidesPerView: 1,
+            autoplay: 15000,
+            pagination: _paginator,
+            nextButton: '.slider__next',
+            prevButton: '.slider__prev',
+            loop: true,
+            paginationClickable: true
+        });
     }
+
+    if (_obj.hasClass('reviews__wrap')) {
+        var _slider1 = new Swiper(_obj, {
+            nextButton: '.reviews__next',
+            prevButton: '.reviews__prev',
+            autoplay: 10000,
+            loop: true,
+            paginationClickable: true
+
+        });
+
+    }
+
+    //public properties
+
+    //public methods
+
+    _init();
 };
 
 $(window).on({
